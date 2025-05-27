b8e096ecb0a8c0c8147074e7173929e887505d45
SJM-coding <greatspirit637@gmail.com>
2025-05-27 15:12:00 +0900

    chore: 파일 위치 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
-import About from './pages/About';
 
 export default function App() {
   return (
@@ -11,13 +10,11 @@ export default function App() {
       {/* 네비게이션 바 */}
       <nav style={{ padding: '12px', borderBottom: '1px solid #ccc' }}>
         <Link to="/" style={{ marginRight: '1rem' }}>홈</Link>
-        <Link to="/about">소개</Link>
       </nav>
 
       {/* 라우팅 영역 */}
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
 
         {/* 존재하지 않는 경로 */}
         <Route path="*" element={
